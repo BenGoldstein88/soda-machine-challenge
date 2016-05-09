e07ba31a030b6d50eaad50eed4ec1d64398ccd92
Ben Goldstein <email": null>
2016-05-09 14:54:59 -0700

    refactored, cleanup

--- soda_machine.rb
+++ soda_machine.rb
@@ -11,51 +11,19 @@ class SodaMachine
     @sodas.length
   end
 
-  # def find_soda(soda_brand)
-  #   @sodas.each do |soda|
-  #     if soda.brand == soda_brand
-  #       return soda
-  #     end
-  #   end
-  #   nil
-  # end
-
   def find_soda(soda_brand)
     @sodas.find { |soda| soda.brand == soda_brand }
   end
 
-def sell(soda_brand)
-  if find_soda(soda_brand) != nil
-    current_soda = @sodas.find{ |soda| soda.brand == soda_brand}
-    @cash += current_soda.price
-    @sodas.delete(current_soda)
+  def sell(soda_brand)
+    if find_soda(soda_brand) != nil
+      current_soda = @sodas.find{ |soda| soda.brand == soda_brand}
+      @cash += current_soda.price
+      @sodas.delete(current_soda)
+    end
   end
-end
 
-  # def sell(soda_brand)
-  #   if find_soda(soda_brand) != nil
-  #     @sodas.each do |soda|
-  #       if soda.brand == soda_brand
-  #         # add cash
-  #         @cash += soda.price
-  #         # remove soda from machine
-  #         @sodas.delete(soda)
-  #         return nil
-  #       end
-  #     end
-  #   end
-  #   nil
-  # end
 
 end
 
-# a = Soda.new(brand: 'Mt. Dew', price: 0.65)
-# b = Soda.new(brand: 'Coke', price: 0.65)
-# c = Soda.new(brand: 'Pepsi', price: 0.65)
-# s = SodaMachine.new(sodas: [a,b,c], cash: 1.00)
-
-
-# puts s.sodas.to_s
-
-# s.find_soda('Pepsi')
 
